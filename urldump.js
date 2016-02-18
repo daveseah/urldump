6f4f6bcfdc2274c3b5b0dbe502d1e0dcccee32e2
Dave Seah <david@davidseah.com>
2016-02-18 00:38:12 -0500

    add stub urldump

---
+++ urldump.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+    var path = require('path');
+var pkg = require(path.join(__dirname,'package.json'));
+var program = require ('commander');
+
+program
+  .version(pkg.version)
+  .parse(process.argv);
+  
+console.log('urldump');
