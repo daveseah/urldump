43df7c5ef7717f8a0cf7936f63640998202a6396
Dave Seah <david@davidseah.com>
2016-02-19 00:41:49 -0500

    First Release parsing INI-style .url (Windows) and XML-encoded .webloc (Mac OS X)

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ var path = require('path');
 var fs = require('fs');
 var pkg = require(path.join(__dirname,'package.json'));
 var program = require ('commander');
+var xml2js = require('xml2js');
+var util = require('util');
+var ini = require('ini');
 
 
 /// DECLARATIONS //////////////////////////////////////////////////////////////
@@ -37,7 +40,9 @@ fs.readdir( wd, function( err,files ) {
 	files.forEach(function(name){
 		var filePath = path.join(wd,name);
 		var stats = fs.statSync(filePath);
-		if (stats.isFile()) ProcessFile(filePath);
+		var out = '';
+		if (stats.isFile()) ProcessFile(filePath, stats);
+		console.log(out);
 	});
 	console.log('Found',num_webloc,'.webloc files and',num_url,'.url files');
 });
@@ -47,22 +52,47 @@ fs.readdir( wd, function( err,files ) {
 ///	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 /*/ given a valid filePath, determine if it's a file to parse of one of two
 	XML types based on extension
-/*/	function ProcessFile ( filePath ) {
-		switch (path.extname( filePath )) {
-			case '.webloc': 	ParseDotWebloc( filePath );
-								break;
-			case '.url': 		ParseDotUrl( filePath );
-								break;
-			default: 			return;
-		}
+/*/	function ProcessFile ( filePath, stats ) {
+			switch (path.extname( filePath )) {
+				case '.webloc': 	ParseDotWebloc( filePath, stats );
+									break;
+				case '.url': 		ParseDotUrl( filePath, stats );
+									break;
+				default: 			return;
+			}
 	}
 ///	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 /*/	Parse XML file in Webloc Format
-/*/	function ParseDotWebloc ( filePath ) {
+/*/	function ParseDotWebloc ( filePath, stats ) {
+		var parser = new xml2js.Parser();
+		var data = fs.readFileSync(filePath,'utf8');
+		parser.parseString( data, function( err, result ) {
+			if (err) {
+				console.log(filePath,'error',err);
+				return;
+			} 
+			var url = result.plist.dict[0].string[0];
+			var birth = stats.birthtime;
+			var mm = ("0"+(birth.getMonth()+1)).slice(-2);
+			var dd = ("0"+birth.getDate()).slice(-2);
+			var yyyy = birth.getFullYear();
+			var date = yyyy+'/'+mm+'/'+dd;
+			var out = '* '+date+' ['+path.basename(filePath,'.webloc')+']('+url+')';
+			console.log(out);
+		});
 		num_webloc++;
 	}
 ///	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 /*/	Parse XML file in URL format
-/*/	function ParseDotUrl ( filePath ) {
+/*/	function ParseDotUrl ( filePath, stats ) {
+		var result = ini.parse(fs.readFileSync(filePath,'utf-8'));
+		var url = result.InternetShortcut.URL;
+		var birth = stats.birthtime;
+		var mm = ("0"+(birth.getMonth()+1)).slice(-2);
+		var dd = ("0"+birth.getDate()).slice(-2);
+		var yyyy = birth.getFullYear();
+		var date = yyyy+'/'+mm+'/'+dd;
+		var out = '* '+date+' ['+path.basename(filePath,'.url')+']('+url+')';
+		console.log(out);
 		num_url++;
 	}
